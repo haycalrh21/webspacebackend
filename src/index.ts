f63857a7fb68872507e320c8dfbbe88c989ca6e8
Your Name <hrayhansyah@gmail.com>
2024-11-05 19:48:30 +0700

    check origin

--- src/index.ts
+++ src/index.ts
@@ -15,47 +15,55 @@ const port = 4000;
 const app = express();
 app.use(cookieParser());
 app.use(urlencoded({ extended: false, limit: "50mb" }));
-// app.use(cors());
+app.use(json({ limit: "50mb" }));
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://webspacefrontend.vercel.app",
+];
+
+// Middleware CORS yang diperbarui
+app.use(
+  cors({
+    origin: allowedOrigins, // Menentukan origin yang diizinkan
+    credentials: true, // Mengizinkan cookies
+  })
+);
+
+// Middleware untuk memvalidasi origin secara manual
 const checkOrigin = (req: Request, res: Response, next: NextFunction) => {
   const origin = req.headers.origin;
   const appOrigin = req.headers["x-app-origin"]; // Header kustom untuk React Native
 
-  console.log(origin, appOrigin);
+  console.log("Origin:", origin, "App Origin:", appOrigin);
 
   if (
-    origin === "http://localhost:3000" ||
-    origin === "https://webspacefrontend.vercel.app" ||
-    appOrigin === "react-native-app" // Menambahkan pengecekan untuk React Native
+    allowedOrigins.includes(origin ?? "") || // Memeriksa jika origin termasuk yang diizinkan
+    appOrigin === "react-native-app" // Pengecekan untuk React Native
   ) {
-    next(); // Lanjutkan ke rute jika asal valid
+    next();
   } else {
     res.status(403).send("Origin not allowed");
   }
 };
 
-app.use(
-  cors({
-    credentials: true,
-  })
-);
-app.use(json({ limit: "50mb" }));
-
+// Route configuration
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-// routes
+
 app.use("/project", checkOrigin, projectRouter);
-app.use("/auth", authRouter);
-app.use("/blog", blogRouter);
-app.use("/bot", botRouter);
-app.use("/discuss", discussRouter);
-app.use("/comment", commentRouter);
-app.use("/tasks", tasksRouter);
+app.use("/auth", checkOrigin, authRouter);
+app.use("/blog", checkOrigin, blogRouter);
+app.use("/bot", checkOrigin, botRouter);
+app.use("/discuss", checkOrigin, discussRouter);
+app.use("/comment", checkOrigin, commentRouter);
+app.use("/tasks", checkOrigin, tasksRouter);
 
 if (process.env.NODE_ENV === "dev") {
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
   });
 }
+
 export const handler = serverless(app);
